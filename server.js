8bdcd0aef020f71bd34180187f134a2973c86827
rmhancock <rosshancock91@gmail.com>
2019-11-18 13:00:56 +0000

    local database variables set

--- server.js
+++ server.js
@@ -5,11 +5,12 @@ const path = require('path');
 const parser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
 const createRouter = require('./router.js');
+const filter = require('content-filter');
 
 const publicPath = path.join(__dirname, '../client/public');
 app.use(express.static(publicPath));
-
 app.use(parser.json())
+app.use(filter())
 
 let corsOptions = {
     origin: "*",
@@ -19,16 +20,17 @@ app.use(cors(corsOptions));
 
 // process.env.MONGODB_URI
 // 'mongodb://localhost:27017'
-// ds053978.mlab.com:53978/heroku_g4jmjlqw
+// ds053978.mlab.com:53978/
+// heroku_g4jmjlqw
 
 
-MongoClient.connect(process.env.MONGODB_URI, {
+MongoClient.connect('mongodb://localhost:27017', {
         useNewUrlParser: true,
         useUnifiedTopology: true
     })
     .then((client) => {
         console.log('database started');
-        const db = client.db('heroku_g4jmjlqw');
+        const db = client.db('formDatabase');
         const formCollection = db.collection('formCollection');
         const formRouter = createRouter(formCollection);
         app.use('/', formRouter);
